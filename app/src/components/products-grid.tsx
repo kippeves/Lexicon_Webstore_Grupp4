e726d5cb01d2adc59ea82229497d0d978213c424
Christoffer <nordin.christoffer@gmail.com>
2025-09-10 11:25:11 +0200

    Customizable component class

--- app/src/components/products-grid.tsx
+++ app/src/components/products-grid.tsx
@@ -1,10 +1,12 @@
 import { ThinProduct } from "@/lib/types";
 import ProductCard from "./product-card";
 
-export default function ProductsGrid({ products, title, emptyText = "Inga produkter hittades." }: { products: ThinProduct[], title?: string, emptyText?: string }
+export default function ProductsGrid(
+    { products, title, emptyText = "Inga produkter hittades.", className }:
+        { products: ThinProduct[], title?: string, emptyText?: string, className?: string }
 ) {
     return (
-        <div>
+        <div className={className}>
             {title ? <h2 className="mb-4 font-bold">{title}</h2> : ''}
             <ul className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] justify-center gap-2">
                 {products.map((product) => (
