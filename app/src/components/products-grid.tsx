4bd48046767390e671d66cdb02ce2f22494f49a6
Christoffer <nordin.christoffer@gmail.com>
2025-09-10 11:18:53 +0200

    Rebase, adds ProductCard, optional list title

--- app/src/components/products-grid.tsx
+++ app/src/components/products-grid.tsx
@@ -1,7 +1,18 @@
 import { ThinProduct } from "@/lib/types";
+import ProductCard from "./product-card";
 
-export default function ProductsGrid({ products }: { products: ThinProduct }) {
+export default function ProductsGrid({ products, title }: { products: ThinProduct[], title?: string }
+) {
     return (
-        <ul className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] justify-center gap-4"></ul>
+        <div>
+            {title ? <h2 className="mb-4 font-bold">{title}</h2> : ''}
+            <ul className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] justify-center gap-2">
+                {products.map((product) => (
+                    <li key={product.id}>
+                        <ProductCard product={product}></ProductCard>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
