e48f42b426cfa4c8859c081edb59aa9304f9c18e
Screenbottle <arvid.rydh@gmail.com>
2025-09-11 14:54:43 +0200

    more improvements

--- app/src/app/products/[id]/page.tsx
+++ app/src/app/products/[id]/page.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 import ProductPrice from "@/components/product-price";
 import StockStatus from "@/components/stock-status";
 
-export default async function ProductsDetailPage({ searchParams }: {searchParams: Promise<{ query?: string }>}) {
-  const { query } = await searchParams;
-  const productId = query ? Number(query) : undefined;
+export default async function ProductPage(props: { params: Promise<{ [key: string]: string | undefined }> }) {
+  const params = await props.params;
+  const productId = params.id ? Number(params.id) : undefined;
 
 
   if (typeof productId !== "number" || isNaN(productId)) {
@@ -14,22 +14,24 @@ export default async function ProductsDetailPage({ searchParams }: {searchParams
   }
   const product = await getProduct(productId);
 
-  return(
-    <div className="flex flex-row gap-4 p-4">
-      <Image src={product.images[0]} alt={product.title} width={200} height={200} />
+  return (
+    <div className="flex flex-row gap-4 p-4 bg-white rounded-lg">
+      <Image src={product.images[0]} alt={product.title} width={500} height={500} />
       <div className="flex flex-col">
         <h1 className="text-2xl font-bold mb-2">{product.title}</h1>
         <p className="text-lg font-semibold mb-2">Rating: {product.rating} ⭐</p>
         <ProductPrice price={product.price} discountPercentage={product.discountPercentage} />
-        <p className="text-lg font-semibold mb-2">Description: {product.description}</p>
+        <p className="text-md  mb-2">Description: {product.description}</p>
         <p className="text-lg font-semibold mb-2">Category: {product.category}</p>
         <p className="text-lg font-semibold mb-2">Brand: {product.brand}</p>
         <StockStatus availabilityStatus={product.availabilityStatus} />
+
+        <div className="flex flex-col ml-auto justify-between">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded mb-2 hover:bg-blue-600">Add to Cart</button>
+          <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Buy Now</button>
+        </div>
       </div>
-      <div className="flex flex-col ml-auto justify-between">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded mb-2 hover:bg-blue-600">Add to Cart</button>
-        <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Buy Now</button>
-      </div>
+
     </div>
   );
 }
