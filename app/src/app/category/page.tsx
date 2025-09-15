ba51c16cd810ecf14166834752a7d0d952ffcf3a
Alexander <32795918+Alexeri@users.noreply.github.com>
2025-09-15 14:01:12 +0200

    simple category page + category grid component

---
+++ app/src/app/category/page.tsx
@@ -0,0 +1,9 @@
+import CategoryGrid from "@/components/category-grid";
+
+export default function CategoryPage() {
+  return (
+    <div className="bg-white p-6 rounded">
+      <CategoryGrid />
+    </div>
+  );
+}
