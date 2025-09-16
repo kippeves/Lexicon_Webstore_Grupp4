cc9aba3632dd378c7091587a777b1ce2c5fd4559
Kristian Väisänen <kippeves@gmail.com>
2025-09-16 14:13:24 +0200

    Bugfix: Fixed Product-Image getting cut off.

--- app/src/components/product-card.tsx
+++ app/src/components/product-card.tsx
@@ -4,25 +4,27 @@ import ProductPrice from "./product-price";
 import StockStatus from "./stock-status";
 
 export default function ProductCard({ product }: { product: ThinProduct }) {
-    return (
-        <article className="grid grid-rows-subgrid gap-3 row-span-5 border rounded-xs py-2 px-4 shadow">
-            <div className="flex flex-col items-center mb-2">
-                <Image
-                    src={product.thumbnail}
-                    alt={product.title}
-                    className="w-full h-48 object-cover rounded"
-                    width={300}
-                    height={300}
-                />
-                <p className="text-sm text-gray-500">{product.rating} ⭐</p>
-            </div>
+  return (
+    <article className="grid grid-rows-subgrid gap-3 row-span-5 border rounded-xs py-2 px-4 shadow">
+      <div className="flex flex-col items-center mb-2">
+        <Image
+          src={product.thumbnail}
+          alt={product.title}
+          className="w-full object-scale-down rounded"
+          width={300}
+          height={300}
+        />
+        <p className="text-sm text-gray-500">{product.rating} ⭐</p>
+      </div>
 
+      <h2 className="text-lg font-semibold">{product.title}</h2>
 
-            <h2 className="text-lg font-semibold">{product.title}</h2>
+      <ProductPrice
+        price={product.price}
+        discountPercentage={product.discountPercentage}
+      />
 
-            <ProductPrice price={product.price} discountPercentage={product.discountPercentage} />
-
-            <StockStatus availabilityStatus={product.availabilityStatus} />
-        </article>
-    )
+      <StockStatus availabilityStatus={product.availabilityStatus} />
+    </article>
+  );
 }
