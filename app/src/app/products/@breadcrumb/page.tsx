486d324b37374b40e8a9f4af48720975a67a8228
Alexander <32795918+Alexeri@users.noreply.github.com>
2025-09-18 16:35:33 +0200

    added breadcrumbs to category route, shows category on product detail page, new breadcrumb component

--- app/src/app/products/@breadcrumb/page.tsx
+++ app/src/app/products/@breadcrumb/page.tsx
@@ -1,26 +1,8 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+import { Breadcrumbs } from "@/components/breadcrumbs";
 
 export default function BreadcrumbSlot() {
+  const links = [{ href: "/", title: "Home" }];
   return (
-    <div className="bg-white p-4 rounded">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href={"/"}>Home</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage className="capitalize">Products</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-    </div>
+    <Breadcrumbs links={links} current="Products" />
   );
 }
