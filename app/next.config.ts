7fc3ce1e6a0c964ebfa0410fbe20bd1ccc174fb7
Kristian Väisänen <kippeves@gmail.com>
2025-09-18 21:16:59 +0200

    Update Image-cache to last 7 days instead of 1 minute.

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    minimumCacheTTL: 604800, // 7 days
     remotePatterns: [new URL('https://cdn.dummyjson.com/product-images/**')],
   },
-  /* config options here */
 };
 
 export default nextConfig;
