2cb6471dcb266ab13c5b1af98b49f364035a3497
Christoffer <nordin.christoffer@gmail.com>
2025-09-18 11:13:46 +0200

    Further fixes and refactor

--- app/src/components/products/filter/price-slider.tsx
+++ app/src/components/products/filter/price-slider.tsx
@@ -1,31 +1,29 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Slider } from "@/components/ui/slider";
 import { ReadonlyURLSearchParams } from "next/navigation";
 import { useState } from "react";
+import FilterCard from "./filter-card";
 
 export default function PriceSlider({
   onRangeUpdate,
   params,
   values,
-  className = "",
-  title = ""
 }: {
   onRangeUpdate: (index: string, value: string[]) => void;
   params: ReadonlyURLSearchParams;
   values: number[];
-  className?: string;
   title?: string;
 }) {
 
-  const paramsPrice = params.get("price")!.split(",") ?? [];
-  const prices = paramsPrice.map(val => Number(val));
+  const prices = values.map(val => Number(val));
 
   const rangeMin = prices ? Math.min(...prices) : 0;
   const rangeMax = prices ? Math.max(...prices) : 0;
-  const selectedMin = Number(values[0]);
-  const selectedMax = Number(values[1]);
+
+  const paramsPrice = params.has("price") ? params.get("price")!.split(",") : [];
+  const selectedMin = paramsPrice[0] ? Number(paramsPrice[0]) : 0;
+  const selectedMax = paramsPrice[1] ? Number(paramsPrice[1]) : 0;
 
   const [selected, setSelected] = useState([selectedMin, selectedMax]);
 
@@ -38,11 +36,8 @@ export default function PriceSlider({
   }
 
   return (
-    <Card className={`${className} gap-2 border-0 px-2 rounded-none shadow-none`}>
-      <CardHeader className="px-0">
-        <CardTitle className="text-lg font-semibold">{title}</CardTitle>
-      </CardHeader>
-      <CardContent className="p-0">
+    <FilterCard title={"Price"}>
+      <div>
         <div className="flex flex-row mb-2 justify-between">
           <div>{rangeMin}</div>
           <div>{rangeMax}</div>
@@ -56,7 +51,7 @@ export default function PriceSlider({
           <div>{selected[0]}</div>
           <div>{selected[1]}</div>
         </div>
-      </CardContent>
-    </Card>
+      </div>
+    </FilterCard>
   );
 }
