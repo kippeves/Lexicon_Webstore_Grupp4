c24cdc217c76ea65f7b840b9ff3b1252281508a1
Christoffer <nordin.christoffer@gmail.com>
2025-09-24 17:11:22 +0200

    Adds brands to create page

--- app/src/app/admin/create/page.tsx
+++ app/src/app/admin/create/page.tsx
@@ -1,13 +1,16 @@
-"use client";
 import { ContentWrapper } from "@/components/content-wrapper";
-import ProductForm from "../../../components/admin/product-form";
+import ProductForm from "@/components/admin/product-form";
 import { Create } from "../actions";
+import { getBrandsByProducts } from "@/lib/data/products";
 
-export default function CreateProductPage() {
+export default async function CreateProductPage() {
+
+  const brands = await getBrandsByProducts();
   return (
     <ContentWrapper>
       <ProductForm
         initialState={{}}
+        brands={brands}
         serverAction={Create}
         submitButtonText='Add Product'
       />
